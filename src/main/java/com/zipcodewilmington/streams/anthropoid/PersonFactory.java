8e8327c981340d4200e598afc3a9a45c8f9f78a0
ule270 <u.le270@gmail.com>
2023-04-14 16:39:19 -0400

    Person Factory tests passing.

--- src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
+++ src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
@@ -39,7 +39,8 @@ public final class PersonFactory {
      * @return - ArrayList of Person objects
      */ // TODO
     public List<Person> createPersonList(int listSize) {
-        return null;
+        //creates a new list using collectors
+        return createPersonStream(listSize).collect(Collectors.toList());
     }
 
 
@@ -48,7 +49,7 @@ public final class PersonFactory {
      * @return - Array of Person objects
      */ // TODO
     public Person[] createPersonArray(int arrayLength) {
-        return null;
+        return createPersonList(arrayLength).toArray(new Person[arrayLength]);
     }
 
 
@@ -59,6 +60,8 @@ public final class PersonFactory {
      * @return - Stream representation of collection of Person objects
      */ // TODO
     public Stream<Person> createPersonStream(int streamCount) {
-        return null;
+        //calling method from above to generate a random person
+        //until you hit limit streamCount;
+        return Stream.generate(this::createRandomPerson).limit(streamCount);
     }
 }
